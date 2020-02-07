b567d7d33ab5ebde14b99447e09d603e4e47e50b
James Taylor <j.taylor@kainos.com>
2020-02-07 15:30:17 +0000

    Messy project with converting json to csv after reading excel

---
+++ README.md
@@ -0,0 +1,8 @@
+A messy packaga.json to csv file converter
+
+1. Copy contents of current table (including header) to excel and save as an xlsx file
+2. Install dependnecies `npm install`
+3. Run e.g `node index.js inputExcelFileName outputFileName jsonfilepath/package.json`
+4. Open outputted csv file in excel and copy/paste to contents of table again
+
+Tips: Don't add extensions to inputExcelFileName and outputFileName
---
+++ index.js
@@ -0,0 +1,57 @@
+//USAGE: node index.js  currentExcelFileName outputFileName jsonfilepath
+console.warn('USAGE: node index.js  currentExcelFileName outputFileName jsonfilepath/package.json')
+console.log('Do not include file extensions apart from package.json')
+console.log('Input is xlsx format and output is csv')
+const fs = require('fs')
+
+
+const currentExcelFile = process.argv[2]
+const outputFile = process.argv[3]
+const relativeJsonFilePath = process.argv[4]
+
+const json = require(relativeJsonFilePath)
+
+const excelToJson = require('convert-excel-to-json')
+
+const result = excelToJson({
+    sourceFile: `${currentExcelFile}.xlsx`,
+    header: {
+      rows: 1
+    },
+    columnToKey: {
+        A: 'name',
+        B: 'B',
+        C: 'C',
+        D: 'description'
+    }
+})
+
+const devOrProduction = (type) => {
+  return type === 'dependencies' ? 'Production' : 'Dev'
+}
+
+const getCSVStringFromDependency = (type) => {
+  let returnString = ''
+  Object.entries(json[type]).forEach(dependency => {
+    const currentValue = result.Sheet1.find(row => row.name === dependency[0])
+
+    if (dependency[0].includes("@")) {
+      dependency[0] = dependency[0].replace("@", "")
+    }
+
+
+    if (currentValue) {
+      returnString += `"${dependency[0]}",${dependency[1]},${devOrProduction(type)},${currentValue.description}\n`
+    } else {
+      returnString += `"${dependency[0]}",${dependency[1]},${devOrProduction(type)},\n`
+    }
+  })
+
+  return returnString
+}
+
+const returnString = getCSVStringFromDependency('dependencies') + getCSVStringFromDependency('devDependencies')
+
+fs.writeFileSync(`${outputFile}.csv`, returnString, 'binary')
+
+console.log('DONE')
